0b6d1c1a1758e2f4abf366efed39bbf6585fd5b8
dng <dominicnzl@gmail.com>
2019-07-11 22:04:15 +0200

    foutje gecorrigeerd

---
+++ src/main/java/nl/ktmc/mongodbreactivestockquoteservice/MongodbReactiveStockQuoteServiceApplication.java
@@ -0,0 +1,13 @@
+package nl.ktmc.mongodbreactivestockquoteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbReactiveStockQuoteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbReactiveStockQuoteServiceApplication.class, args);
+	}
+
+}
